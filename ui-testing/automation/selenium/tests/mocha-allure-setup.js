78ec27afcd7ad67a30e43b8d688aa938757db061
Puzz1eAssemb1er <sergeygre4kin@gmail.com>
2026-03-29 22:43:27 +0300

    Update mocha-allure-setup.js

--- ui-testing/automation/selenium/tests/mocha-allure-setup.js
+++ ui-testing/automation/selenium/tests/mocha-allure-setup.js
@@ -3,6 +3,3 @@ const allure = require('allure-mocha/runtime');
 beforeEach(function () {
   allure.label('framework', 'selenium');
 });
-
-
-mocha tests/**/*.spec.js --require tests/mocha-allure-setup.js
