3c25ca59621b526cf57a456af67705cd6aa313c7
Puzz1eAssemb1er <sergeygre4kin@gmail.com>
2026-03-29 23:17:31 +0300

    Delete Создай файл: ui-testing/automation/selenium/tests/mocha-allure-setup.js

--- Создай файл: ui-testing/automation/selenium/tests/mocha-allure-setup.js
+++
@@ -1,8 +0,0 @@
-const allure = require('allure-mocha/runtime');
-
-beforeEach(function () {
-  allure.label('framework', 'selenium');
-});
-
-
-mocha tests/**/*.spec.js --require tests/mocha-allure-setup.js
