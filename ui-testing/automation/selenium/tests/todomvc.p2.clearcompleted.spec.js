a907f4f3ac0c9d7cc4e4991f69a7d6d2b2c2c334
Puzz1eAssemb1er <sergeygre4kin@gmail.com>
2026-03-30 18:53:48 +0300

    Update todomvc.p2.clearcompleted.spec.js

--- ui-testing/automation/selenium/tests/todomvc.p2.clearcompleted.spec.js
+++ ui-testing/automation/selenium/tests/todomvc.p2.clearcompleted.spec.js
@@ -1,6 +1,6 @@
 var assert = require('assert');
 var driver = require('../utils/driver').driver;
-var TodoPage = require('../pages/todomvc.page').TodoPage;
+var TodoPage = require('../pages/todomvc.page.cjs').TodoPage;
 
 describe('P2: Clear completed', function () {
   var page;
